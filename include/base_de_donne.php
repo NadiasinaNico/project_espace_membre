9425cd2da7d4e4ab56a56fdd6de4f7a84066afe0
NadiasinaNico <nadiasinanicodev@gmail.com>
2022-01-11 21:01:15 +0300

    insert data

---
+++ include/base_de_donne.php
@@ -0,0 +1,8 @@
+<?php
+
+
+$my_base_de_donne = new PDO("mysql:host=localhost;dbname=project_espace_membres","root", "Aslandonance1313");
+$my_base_de_donne->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+
+?>
